8fea55dc37a15d7a87d20d6934af07c0495094c6
Thimo <thimo2000@hotmail.nl>
2018-03-26 11:30:51 +0200

    project
    project
    
    Co-Authored-By: RutgerK01 <rutgerk01@users.noreply.github.com>

---
+++ home.php
@@ -0,0 +1,77 @@
+<main class="container">
+  <div class="row">
+    <div class="col-sm-8">
+      <h2 class="mt-4">Welkom op mijn site</h2>
+      <p>Dementie Nederland is er voor u. We werken aan een toekomst zonder dementie en aan een betere kwaliteit van leven voor mensen met dementie en hun dierbaren. Daarom bieden we informatie en advies, komen we op voor betere zorg en investeren we in wetenschappelijk onderzoek.</p>
+      <p></p>
+    </div>
+    <div class="col-sm-4">
+      <h2 class="mt-4">Contact</h2>
+        <address>
+          <strong>MBO Utrecht</strong>
+          <br>
+          Columbuslaan 540
+          <br>
+          3526 EP Utrecht
+          <br>
+          Utrecht, NL
+          <br>
+        </address>
+        <address>
+          <abbr title="Phone">Telefoon:</abbr>
+          030 281 5150
+          <br>
+          <abbr title="Email">Email:</abbr>
+          <a href="mailto:#">Thimo2000@hotmail.nl</a>
+        </address>
+    </div>
+  </div>
+
+    <div class="wrap">
+      <div class="tile"> 
+        <img src='./images/dementie_card_1.jpg'/>
+        <div class="text">
+        <h1>Alzheimer</h1>
+        <h2 class="animate-text">More lorem ipsum bacon ipsum.</h2>
+        <p class="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="tile"> 
+    <img src='./images/dementie_card_2.jpg'/>
+    <div class="text">
+    <h1>Lorem ipsum.</h1>
+    <h2 class="animate-text">More lorem ipsum bacon ipsum.</h2>
+    <p class="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+  <div class="dots">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    </div>
+  </div>
+    
+    <div class="tile"> 
+    <img src='./images/dementie_card_3.jpg'/>
+    <div class="text">
+    <h1>Lorem ipsum.</h1>
+    <h2 class="animate-text">More lorem ipsum bacon ipsum.</h2>
+    <p class="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+  <div class="dots">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    </div>
+  </div>
+  </div>
+    
+    
+  </div>
+</main>
